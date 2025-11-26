8941da68c5137d3be21110e7a1d8fbe238356f5a
Moazzem-Hossain-Sowrov <145192654+Moazzem-Hossain-Sowrov@users.noreply.github.com>
2025-11-27 05:14:44 +0600

    serviceDetails

--- src/pages/ServiceDetails.jsx
+++ src/pages/ServiceDetails.jsx
@@ -1,37 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import servicesData from "../../public/services.json";
+
 
 const ServiceDetails = () => {
 
   const [services, setServices] = useState([]);
-  const [serviceDetails, setServiceDetails] = useState([null])
+  const [serviceDetails, setServiceDetails] = useState(null);
+
   const { myId } = useParams()
 
-  useEffect(() => {
-    fetch('/services.json')
-      .then(res => res.json())
-      .then(data => setServices(data))
-      .catch(err => console.log(err))
-  }, [])
+ useEffect(() => {
+  setServices(servicesData);
+}, []);
 
 
 
-  useEffect(() => {
-    if (services.length > 0) {
-      const findResult = services.find(
-        service => String(service.serviceId) === String(myId)
-      );
-      setServiceDetails(findResult);
-    }
-  }, [myId, services]);
-  console.log(serviceDetails);
+useEffect(() => {
+  if (services.length > 0) {
+    const result = services.find(service => service.serviceId == myId);
+    setServiceDetails(result);
+  }
+}, [myId, services]);
+
 
 
 
 
   return (
     <div>
-      <img src={serviceDetails?.image} alt="" />
+     <img src={serviceDetails?.image} alt="" />
+
 
     </div>
   );
